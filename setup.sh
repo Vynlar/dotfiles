b1cb5d1271695c35c25cd1289a7562735c70f67e
aaleixandre <aaleixandre@aviatainc.com>
2016-06-14 13:37:02 -0600

    added YouCompleteMe compilation to setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+
+git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+sudo apt-get install -y cmake
+python ~/dotfiles/.vim/bundle/YouCompleteMe/install.py
