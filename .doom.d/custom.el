0c1ff347d8bffacb8a5c1f94b07ec21e41bd740e
Adrian Aleixandre <aaleixandre@bushelpowered.com>
2020-09-17 09:47:24 -0500

    add doom.d

---
+++ .doom.d/custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   (quote
+    ((cider-clojure-cli-global-options . "-A:dev")
+     (cider-shadow-cljs-default-options . "app")
+     (cider-default-cljs-repl . shadow)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ivy-minibuffer-match-face-1 ((t (:foreground "#aaaaaa")))))
